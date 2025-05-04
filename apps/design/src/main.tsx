9d3914dfde342d0d182518e1fc79cfc3da836ae7
hujinconghu <1843275605@qq.com>
2025-05-04 21:38:01 +0800

    fix: design web

--- apps/design/src/main.tsx
+++ apps/design/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'tdesign-react/dist/tdesign.css';
-import App from './App.tsx'
+import App from './App.js'
 
 import './index.css'
 
